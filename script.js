5499eda90e23bf753b75f2b66c6b33ab7eb8771f
Rohan Vidhate <49265832+roneya@users.noreply.github.com>
2023-04-13 22:32:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-//your JS code here. If required.
+function showDimensions() {
+        var width = window.innerWidth;
+        var height = window.innerHeight;
+        document.getElementById("dimensions").innerHTML = "Width: " + width + "px and Height: " + height + "px";
+      }
+      window.onload = showDimensions; // Show dimensions on page load
+      window.onresize = showDimensions;
